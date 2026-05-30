7bf430f4a4ee61c2170ad25563212e705ecc5319
Bhumik Kumta <bhumik.kumta23@pcu.edu.in>
2026-05-30 20:57:26 +0300

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=settings.ALLOWED_ORIGINS.split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
